a4d8c8716447dfaa697a34212aec03ba54c334a4
Thein Than Toe <theinthantoe15@gmail.com>
2026-01-05 23:25:30 +0630

    payment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
