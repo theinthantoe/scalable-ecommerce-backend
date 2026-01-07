27342e6581d99dea56d655efa789daca77bfe81d
Thein Than Toe <theinthantoe15@gmail.com>
2026-01-07 23:53:59 +0630

    adding orders

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { PaymentModule } from './payment/payment.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, PaymentModule],
+  imports: [UsersModule, ProductsModule, PaymentModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
